838e8dedea8f574b2d8da988dbca6a8e8af0f6de
김광진 <rhkdrod12@gmail.com>
2022-06-03 17:58:43 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -9,31 +9,66 @@ import Menu, { InsertMenu } from "./Component/Menu";
 import MyGrid, { TstGrid } from "./Component/MyGrid2";
 import { DepthMenu } from "./Component/BasicComponent/DepthMenu";
 import "./CssModule/GlobalStyle.css";
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import MenuGrid from "./Component/TestComp/MenuGrid";
 import { StyleDiv } from "./Component/StyleComp/StyleComp";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/insertMenu" element={<InsertMenu />}></Route>
-          <Route path="/test">
-            <Route path="" element={<MenuGrid />}></Route>
-            <Route path="test1" element={<Test />}></Route>
-          </Route>
-          <Route path="*" element={<UnKnown />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Transition />
+    </BrowserRouter>
   );
 }
 
-const UnKnown = () => {
+const Transition = () => {
+  return (
+    <TransitionRouter>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/insertMenu" element={<InsertMenu />}></Route>
+      <Route path="/test/*" element={<TestNavi />}></Route>
+      <Route path="*" element={<UnKnownPage />}></Route>
+    </TransitionRouter>
+  );
+};
+
+const TransitionRouter = ({ children }) => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("fadeIn");
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransistionStage("fadeOut");
+  }, [location, displayLocation]);
+
+  return (
+    <div
+      className={`${transitionStage}`}
+      onAnimationEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setTransistionStage("fadeIn");
+          setDisplayLocation(location);
+        }
+      }}
+    >
+      <Routes location={displayLocation}>{children}</Routes>
+    </div>
+  );
+};
+
+const TestNavi = () => {
+  return (
+    <TransitionRouter>
+      <Route path="" element={<MenuGrid />}></Route>
+      <Route path="test1" element={<Test />}></Route>
+    </TransitionRouter>
+  );
+};
+
+const UnKnownPage = () => {
   return (
     <StyleDiv inStyle={{ display: "flex", "justify-content": "center " }}>
       <StyleDiv inStyle={{ margin: 30, display: "inline-block", fontSize: "40px" }}>Not Found Page</StyleDiv>
